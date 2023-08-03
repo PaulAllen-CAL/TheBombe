74e741c9d26ca96ad615ea9734ca45c4e850220a
Paul Allen <Paula@calastone.com>
2023-08-03 16:57:37 +0100

    hope

--- Program.cs
+++ Program.cs
@@ -2,7 +2,25 @@
 {
     internal static void Main(string[] args)
     {
-        var input = Console.ReadLine(); ;
-        Console.WriteLine($"I got {input}");
+        var input = Console.ReadLine();
+
+        int r = 0;
+        for (var i = 0; i < input.Length; ++i)
+        {
+            var c = input[i];
+
+            var n = c == ' '
+                ? 26
+                : (int)c - (int)'A';
+
+            n += r;
+
+            var d = n / 9;
+            r = n % 9;
+
+            Console.Write((char)d);
+        }
+
+        Console.WriteLine("");
     }
 }
